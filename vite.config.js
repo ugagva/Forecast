c78c92ee0d76a2e59a34e69994e9ff80fd2ab8c4
ugagva <jgorelova@mail.ru>
2024-10-15 10:38:25 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/forecast/",
+
   plugins:[
     vue(),
   ],
+  publicPath: `/forecast/`,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
